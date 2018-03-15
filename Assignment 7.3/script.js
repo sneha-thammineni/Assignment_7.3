4d0cbd2f14d8944be34a76f2d6b2a028e46cfcf2
sneha-thammineni <36546445+sneha-thammineni@users.noreply.github.com>
2018-03-15 22:05:32 +0530

    Add files via upload

---
+++ Assignment 7.3/script.js
@@ -0,0 +1,66 @@
+for(var i=1;i<=100;i++){
+    
+    
+//Using multiples of three and multiples of five first so that they can be printed as 'Fizzbuzz' else when we start with the multiples of three, 15 will be taken as Fizz. Or if its with 5, then it will be a buzz. 
+    
+     if(i%15===0)
+        console.log(i + " FizzBuzz ");
+    // Using " Fizz Buzz " instead of "FizzBuzz" to increase the length of the string which indeed adds the space
+ 
+      else if(i%3===0)
+        console.log(i  + " Fizz ");
+    else if(i%5===0)
+        console.log( i + " Buzz ");
+    
+    else 
+   
+     console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//    var n = 3;
+//   var j= 1;
+//
+//  for(j=1;j<=i;j++){
+//    var code = n * j;
+//    console.log(code);
+//}
+//   
+
+
+
+
+
+
+
+
+
+
+
+
